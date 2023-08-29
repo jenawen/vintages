9f1d5a568ebf27daf7c2b6aca8d0922cf4d8c0fe
jena adkins <jena.adkins@creditone.com>
2023-08-29 12:05:24 -0700

    screaming crying throwing up, got to filtering down to 1 df

--- test2.py
+++ test2.py
@@ -16,8 +16,9 @@ clist6 = df['AnnualFeeGroup'].unique()
 clist7 = df['OriginalCreditLineRange'].unique()
 
 options = st.multiselect("Select vintages:", clist, key="vintages_selected")
+
 selected = st.sidebar.selectbox("Selected Vintages:", options, key="current_vintage")
-# st.write(selected)
+
 FirstSecond = st.sidebar.selectbox("FirstSecond:",clist1)
 Branding = st.sidebar.selectbox("Branding", clist2)
 Channel = st.sidebar.selectbox("Channel", clist3)
@@ -26,94 +27,96 @@ Association = st.sidebar.selectbox("Association", clist5)
 AnnualFeeGroup = st.sidebar.selectbox("AnnualFeeGroup", clist6)
 OriginalCreditLineRange = st.sidebar.selectbox("OriginalCreditLineRange", clist7)
 
-
-
-##Need an empty array to store all the filtered objects 
-frames2 = []
-
 st.header("Vintage Comparison")
 
-#initiate session state of df_result for the dataframe to be updated 
-if 'df_result' not in st.session_state:
-    st.session_state['df_result'] = df
+#initialize default dataframe to hold ALL csv data
+if 'df_default' not in st.session_state:
+    st.session_state['df_default'] = df
+    
+#initialize blank dataframe to hold new df created from user filters
+if 'blank_df' not in st.session_state:
+    st.session_state['blank_df'] = pd.DataFrame(columns=["Vintage", "FirstSecond", 'Branding', 'Channel', 'Source', 'Association', 'AnnualFeeGroup', "OriginalCreditLineRange", "MonthsOnBooks", "NewAccountIndicator", "ActiveAccountIndicator", "PreTaxIncome", "EndingReceivable", "CumlNewAccountIndicator", "CumlActiveAccountIndicator", "CumlPreTaxIncome", "CumlEndingReceivable", "AverageActives", "AverageReceivable", "CumlROA", "CumlROAAnnualized"])
 
-# df.loc[(df['Vintage'].isin(options))]
 
 def main():
     submit = st.sidebar.button('submit')
     if submit:
-        # st.session_state['df_result'] = st.session_state['df_result'].loc[(df['Vintage'] == selected)]
-        st.session_state['df_result'] = st.session_state['df_result'].loc[(df['Vintage'] == selected) & (df['FirstSecond'] == FirstSecond) & (df['Branding'] == Branding) & (df['Channel'] == Channel) & (df['Source'] == Source)
-                    & (df['Association'] == Association) & (df['AnnualFeeGroup'] == AnnualFeeGroup) & (df['OriginalCreditLineRange'] == OriginalCreditLineRange)
-]
-     
+        #filter original df using selected values
+        #create a new df from the filtered entire df
+        df_new = df.loc[(df['Vintage'] == selected) 
+              & (df['FirstSecond'] == FirstSecond) 
+              & (df['Branding'] == Branding) 
+              & (df['Channel'] == Channel) 
+              & (df['Source'] == Source) 
+              & (df['Association'] == Association) 
+              & (df['AnnualFeeGroup'] == AnnualFeeGroup) 
+              & (df['OriginalCreditLineRange'] == OriginalCreditLineRange)]
+        #store this new df into blank_df made earlier
+        st.session_state['blank_df'] = pd.concat([st.session_state['blank_df'], df_new], axis=0)
+        #new dataframe
+        st.dataframe(st.session_state['blank_df'])
 
-df1 = st.session_state['df_result']
+st.write('BLANK DF',st.session_state['blank_df'])
+        
+#assign the df that contains all csv data to df1
+df1 = st.session_state['df_default']
+#assign the new filtered df to df2
+df2 = st.session_state['blank_df']
+#frames list made of the default df and the new filtered df
+frames = [df1, df2]
+#concat these 
+result = pd.concat(frames)
+#write result to screen
+st.write('SUBMITTED',result)
+
+if df2.empty == True:
+    fig1 = px.line(df1.melt(id_vars="Vintage"),x=df1['MonthsOnBooks'], y=df1['ActiveAccountIndicator'], color=df1['Vintage'], markers=True, title='Active Accounts', labels={'y':'Active Accounts', 'x':'Months on Book', "color":"Vintage"})
+    st.plotly_chart(fig1)
+    st.write('FIG 1')
+    
+else:
+    fig2 = px.line(df2.melt(id_vars="Vintage"),x=df2['MonthsOnBooks'], y=df2['ActiveAccountIndicator'], color=df2['Vintage'], markers=True, title='Active Accounts', labels={'y':'Active Accounts', 'x':'Months on Book', "color":"Vintage"})
+    st.plotly_chart(fig2)
+    st.write('FIG 2')
+    
+
+
+
     # st.session_state
 
-if 'df_result2' not in st.session_state:
-    st.session_state['df_result2'] = df
+# if 'df_result2' not in st.session_state:
+#     st.session_state['df_result2'] = df
+
+# def add_to_main():
+#     add = st.sidebar.button('add')
+#     if add:
+#         st.session_state['df_result2'] = st.session_state['df_result2'].loc[(df['Vintage'] == selected) & (df['FirstSecond'] == FirstSecond) & (df['Branding'] == Branding) & (df['Channel'] == Channel) & (df['Source'] == Source)
+#                     & (df['Association'] == Association) & (df['AnnualFeeGroup'] == AnnualFeeGroup) & (df['OriginalCreditLineRange'] == OriginalCreditLineRange) ]
+#         # st.session_state['df_result'].append(df.loc[(df['Vintage'] == selected) & (df['FirstSecond'] == FirstSecond) & (df['Branding'] == Branding) & (df['Channel'] == Channel) & (df['Source'] == Source)
+#         #             & (df['Association'] == Association) & (df['AnnualFeeGroup'] == AnnualFeeGroup) & (df['OriginalCreditLineRange'] == OriginalCreditLineRange)])
+
+# df2 = st.session_state['df_result2']
 
 
-## SUBMIT / ADD ALGORITHM:
-### on add, push the sorted csv data to the frames array
-#### use the options variable to get the number of vintages selected 
-#### use for loop for # of vintages selected
-### on submit, display the frames
 
-##Need to initalize as many session states as # of vintages selected 
-### onClick of add button, append the new filtered object to the array
-for i in enumerate(options):
-    if f"df_result_{i[0]}" not in st.session_state:
-        st.session_state[f"df_result_{i[0]}"] = f"df_{i[0]}"
+# fig2 = px.line(result.melt(id_vars="Vintage"),x=result['MonthsOnBooks'], y=result['CumlROAAnnualized'], color=result['Vintage'], markers=True, title='CumlROAAnnualized', labels={'y':'ROAAnnualized', 'x':'Months on Book', "color":"Vintage"})
 
-def add_to_main():
-    add = st.sidebar.button('add')
-    if add:
-        st.session_state["df_result2"] = st.session_state["df_result2"].loc[(df['Vintage'] == selected) & (df['FirstSecond'] == FirstSecond) & (df['Branding'] == Branding) & (df['Channel'] == Channel) & (df['Source'] == Source)
-                    & (df['Association'] == Association) & (df['AnnualFeeGroup'] == AnnualFeeGroup) & (df['OriginalCreditLineRange'] == OriginalCreditLineRange) ]
-        
-df2 = st.session_state['df_result2']
+# fig2.update_layout(yaxis_ticksuffix = ".3%")
+# st.plotly_chart(fig2)
 
-        
-        # for i in enumerate(options):
-                # st.session_state[f"df_result_{i[0]}"] = st.session_state[f"df_result_{i[0]}"].loc[(df['Vintage'] == selected) & (df['FirstSecond'] == FirstSecond) & (df['Branding'] == Branding) & (df['Channel'] == Channel) & (df['Source'] == Source)
-                #     & (df['Association'] == Association) & (df['AnnualFeeGroup'] == AnnualFeeGroup) & (df['OriginalCreditLineRange'] == OriginalCreditLineRange) ]
-                # frames2.append( st.session_state[f"df_result_{i[0]}"])
-                # st.write('hi', frames2)
-   
-        
-        
-        
+# fig3 = px.line(result.melt(id_vars="Vintage"),x=result['MonthsOnBooks'], y=result['CumlPreTaxIncome'], color=result['Vintage'], markers=True, title='CumlPreTaxIncome', labels={'y':'PreTaxIncome', 'x':'Months on Book', "color":"Vintage"})
+# st.plotly_chart(fig3)
 
-frames = [df1, df2]
-result = pd.concat(frames)
-st.write(result)
-
-fig = px.line(result.melt(id_vars="Vintage"),x=result['MonthsOnBooks'], y=result['ActiveAccountIndicator'], color=result['Vintage'], markers=True, title='Active Accounts', labels={'y':'Active Accounts', 'x':'Months on Book', "color":"Vintage"})
-
-# fig.add_trace(connectgaps=False)
-st.plotly_chart(fig)
-
-fig2 = px.line(result.melt(id_vars="Vintage"),x=result['MonthsOnBooks'], y=result['CumlROAAnnualized'], color=result['Vintage'], markers=True, title='CumlROAAnnualized', labels={'y':'ROAAnnualized', 'x':'Months on Book', "color":"Vintage"})
-
-fig2.update_layout(yaxis_ticksuffix = ".3%")
-st.plotly_chart(fig2)
-
-fig3 = px.line(result.melt(id_vars="Vintage"),x=result['MonthsOnBooks'], y=result['CumlPreTaxIncome'], color=result['Vintage'], markers=True, title='CumlPreTaxIncome', labels={'y':'PreTaxIncome', 'x':'Months on Book', "color":"Vintage"})
-st.plotly_chart(fig3)
-
-fig4 = px.line(result.melt(id_vars="Vintage"),x=result['MonthsOnBooks'], y=result['EndingReceivable'], color=result['Vintage'], markers=True, title='EndingReceivable', labels={'y':'EndingReceivable', 'x':'Months on Book', "color":"Vintage"})
-st.plotly_chart(fig4)
+# fig4 = px.line(result.melt(id_vars="Vintage"),x=result['MonthsOnBooks'], y=result['EndingReceivable'], color=result['Vintage'], markers=True, title='EndingReceivable', labels={'y':'EndingReceivable', 'x':'Months on Book', "color":"Vintage"})
+# st.plotly_chart(fig4)
 
         
     
 if __name__ == "__main__":
     main()
-
    
-if __name__ == "__main__":
-    add_to_main()
+# if __name__ == "__main__":
+#     add_to_main()
 
 
 
